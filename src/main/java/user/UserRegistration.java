6e4db7b208fe8e8dc217e2f6f03fcbda2e145d48
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-18 17:04:09 +0530

    Adding LastName

--- src/main/java/user/UserRegistration.java
+++ src/main/java/user/UserRegistration.java
@@ -24,5 +24,21 @@ public class UserRegistration {
 			return null;
 		}
 	}
-	
+	public String enterLastName(String s) {
+		String name = s;
+		if(name.toUpperCase().charAt(0) == name.charAt(0)) {
+			if(name.length()>2) {
+				return name;
+			}
+			else {
+				System.out.println("Not more the 2 letter!");
+				return null;
+			}
+		}
+		else {
+			System.out.println("Not First letter Capital: ");
+			
+			return null;
+		}
+	}
 }
