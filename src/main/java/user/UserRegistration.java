b80e425ddf7d83ef8cd59e8f10533c5ee1dffe7e
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-19 14:34:47 +0530

    Checking all email

--- src/main/java/user/UserRegistration.java
+++ src/main/java/user/UserRegistration.java
@@ -4,94 +4,121 @@ import java.util.Scanner;
 
 public class UserRegistration {
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	public String checkFirstName(String s) {
 		String name = s;
-		if(name.toUpperCase().charAt(0) == name.charAt(0)) {
-			if(name.length()>2) {
+		if (name.toUpperCase().charAt(0) == name.charAt(0)) {
+			if (name.length() > 2) {
 				return name;
-			}
-			else {
-				System.out.println("Not more the 2 letter!");
+			} else {
 				return null;
 			}
-		}
-		else {
-			System.out.println("Not First letter Capital: ");
-			
+		} else {
 			return null;
 		}
 	}
+
 	public String checkLastName(String s) {
 		String name = s;
-		if(name.toUpperCase().charAt(0) == name.charAt(0)) {
-			if(name.length()>2) {
+		if (name.toUpperCase().charAt(0) == name.charAt(0)) {
+			if (name.length() > 2) {
 				return name;
-			}
-			else {
-				System.out.println("Not more the 2 letter!");
+			} else {
 				return null;
 			}
-		}
-		else {
-			System.out.println("Not First letter Capital: ");
-			
+		} else {
 			return null;
 		}
 	}
-	
-	public String checkUserName(String s) {
-		if(s.indexOf('.')>0 ) {
-			if(s.indexOf('@')>0) {
-				if(s.indexOf('.', s.indexOf('@'))>s.indexOf('@')+1) {
-					return s;
-				}
-				else return null;
-			}else
+
+	public String checkEmail(String s) {
+		if (s.indexOf('.') > 0) {
+			if (s.indexOf('@') > 0) {
+				if (s.indexOf('.', s.indexOf('@')) > s.indexOf('@') + 1) {
+					int coPosi= s.indexOf(".", s.indexOf("@"));
+					if(s.indexOf("co", coPosi)>0) {
+						return s;
+					}
+					return null;
+				} else
+					return null;
+			} else
 				return null;
-		}else
+		} else
 			return null;
-		
+
 	}
-	
+
 	public String checkPhoneNumber(String s) {
-		if(s.indexOf(' ')==2) {
-			if(s.length()-s.indexOf(' ')-1==10) {
+		if (s.indexOf(' ') == 2) {
+			if (s.length() - s.indexOf(' ') - 1 == 10) {
 				return s;
-			}else return null;
-		}else return null;
+			} else
+				return null;
+		} else
+			return null;
 	}
-	
+
 	public String checkPassword(String s) {
-		if(s.length()>7) {
-			int flag = 0;
-			for(int i = 0 ; i<s.length(); i++) {
-				String u = s.toUpperCase();
-				if(s.charAt(i)==u.charAt(i)) {
-					flag=1;
+		if (s.length() > 7) {
+			int flag3 = 0;
+			char[] sp = { '@', '#', '$', '!', '&', '*' };
+			for (int i = 0; i < s.length(); i++) {
+				for (int j = 0; j < sp.length; j++) {
+					if (s.charAt(i) == sp[j]) {
+						flag3 = 1;
+						break;
+					}
 				}
 			}
-			if(flag==1) {
-				int flag2=0;
-				char [] num = {'0','1','2','3','4','5','6','7','8','9'};
-				for(int i = 0; i<s.length(); i++) {
-					for(int j = 0; j< num.length ; j++) {
-						if(s.charAt(i)==num[j]) {
-							flag2=1;
+			if (flag3 == 1) {
+				int flag2 = 0;
+				char[] num = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+				for (int i = 0; i < s.length(); i++) {
+					for (int j = 0; j < num.length; j++) {
+						if (s.charAt(i) == num[j]) {
+							flag2 = 1;
+							break;
 						}
 					}
 				}
-				if(flag2==1) {
-					return s;
+				if (flag2 == 1) {
+					int flag = 0, pass = 0;
+					for (int i = 0; i < s.length(); i++) {
+						pass = 0;
+						for (int j = 0; j < num.length; j++) {
+							if (s.charAt(i) == num[j]) {
+								pass = 1;
+							}
+						}
+						for (int k = 0; k < sp.length; k++) {
+							if (s.charAt(i) == sp[k]) {
+								pass = 1;
+							}
+						}
+						if (pass == 0) {
+							String u = s.toUpperCase();
+							if (s.charAt(i) == u.charAt(i)) {
+								flag = 1;
+								break;
+							}
+						}
+					}
+					if (flag == 1) {
+						return s;
+					}
+
+					return null;
 				}
-				
+
 				return null;
 			}
-			
+
 			return null;
-		}else return null;
+		} else
+			return null;
 	}
-	
+
 }
--- src/test/java/user/UserRegistrationTest.java
+++ src/test/java/user/UserRegistrationTest.java
@@ -9,8 +9,91 @@ public class UserRegistrationTest {
 	UserRegistration c = new UserRegistration();
 		
 	@Test
-	public void shoulBeMoreThen3LetterAndCaptial() {
+	public void firstNameShoulBeMoreThen3LetterAndCaptial() {
 		assertEquals( "Mano" , c.checkFirstName("Mano"));
+	}
+	
+	@Test
+	public void firstNameShoulBeLessThen3LetterAndCaptial() {
+		assertEquals( null , c.checkFirstName("Ma"));
+	}
+	@Test
+	public void firstNameShoulBeLessThen3LetterAndSmallLetter() {
+		assertEquals( null , c.checkFirstName("mano"));
+	}
+	@Test
+	public void lastNameShoulBeMoreThen3LetterAndCaptial() {
+		assertEquals( "Mano" , c.checkLastName("Mano"));
+	}
+	
+	@Test
+	public void lastNameShoulBeLessThen3LetterAndCaptial() {
+		assertEquals( null , c.checkLastName("Ma"));
+	}
+	@Test
+	public void lastNameShoulBeLessThen3LetterAndSmallLetter() {
+		assertEquals( null , c.checkLastName("mano"));
+	}
+	@Test
+	public void emailIsValid() {
+		assertEquals("abc.asd@yo.co.in",c.checkEmail("abc.asd@yo.co.in"));
+	}
+	@Test
+	public void emailIsInvalidWithNoAtTheRate() {
+		assertEquals(null ,c.checkEmail("abc.asdyo.co.in"));
+	}
+	@Test
+	public void emailHavingNoNamefirst() {
+		assertEquals(null ,c.checkEmail(".asd@yo.co.in"));
+	}
+	@Test
+	public void emailHavingNoCompany() {
+		assertEquals(null ,c.checkEmail("abc.asd@.co.in"));
+	}
+	@Test
+	public void emailHavingNoCo() {
+		assertEquals(null ,c.checkEmail("abc.asd@yo.in"));
+	}
+	@Test
+	public void emailHavingNoName() {
+		assertEquals(null ,c.checkEmail("@yo.in"));
+	}
+	@Test
+	public void phoneNumberIsRight() {
+		assertEquals("91 9561092096" ,c.checkPhoneNumber("91 9561092096"));
+	}
+	@Test
+	public void phoneNumberIsInvalidWithoutSpace() {
+		assertEquals(null ,c.checkPhoneNumber("919561092096"));
+	}
+	@Test
+	public void phoneNumberIsInvalidWithLessNumber() {
+		assertEquals(null ,c.checkPhoneNumber("91 95610926"));
+	}
+	@Test
+	public void phoneNumberIsInvalidWithWrongCode() {
+		assertEquals(null ,c.checkPhoneNumber("912 95610926"));
+	}
+	
+	@Test
+	public void passwordIsRight() {
+		assertEquals("Upa@123India",c.checkPassword("Upa@123India"));
+	}
+	@Test
+	public void passwordIsWrongWithLessLetters() {
+		assertEquals(null,c.checkPassword("Upa@12"));
+	}
+	@Test
+	public void passwordIsWrongWithNoUpperCase() {
+		assertEquals(null,c.checkPassword("upa@123india"));
+	}
+	@Test
+	public void passwordIsWrongWithNoNumber() {
+		assertEquals(null,c.checkPassword("Upa@india"));
+	}
+	@Test
+	public void passwordIsWrongWithNoSpecialCharacter() {
+		assertEquals(null,c.checkPassword("Upa123india"));
 	}
 
 }
